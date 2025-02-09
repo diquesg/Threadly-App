edec6d5ef91e35372be099b52fbc34ba96674511
Gustavo Diques <diquesg@gmail.com>
2025-02-09 15:39:25 -0300

    commit

---
+++ threads-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    apiBaseUrl: 'http://localhost:3000',
+    randomUserApiUrl: 'https://usernameapiv1.vercel.app/api/random-usernames',
+    randomIconApiUrl: 'https://api.dicebear.com/9.x/fun-emoji/png?scale=90&radius=50&backgroundColor=fcbc34,d9915b,d84be5,ffdfbf,71cf62,059ff2,c0aede&backgroundType=gradientLinear&backgroundRotation=90,200,280,360,-360&eyes=closed,crying,cute,glasses,love,pissed,plain,sad,shades,sleepClose,stars,tearDrop,wink,wink2,closed2&mouth=cute,kissHeart,lilSmile,plain,smileLol,smileTeeth,tongueOut,wideSmile,drip&seed='
+}

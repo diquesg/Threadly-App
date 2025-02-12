931a8dc442c2262c5453aae2d6d4f7f39512c825
Gustavo Diques <diquesg@gmail.com>
2025-02-12 14:01:45 -0300

    test

--- threads-be/src/comments/comments.gateway.ts
+++ threads-be/src/comments/comments.gateway.ts
@@ -1,12 +1,13 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({ cors: { origin: '*' } }) // Permitir CORS para evitar bloqueios
+@WebSocketGateway({ cors: { origin: '*' } })
 export class CommentsGateway {
     @WebSocketServer()
     server: Server;
 
     sendNewComment(comment: any) {
+        console.log('Emitindo novo comentário:', comment); // Log para depuração
         this.server.emit('newComment', comment);
     }
 }

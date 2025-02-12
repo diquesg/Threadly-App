fd7d8ad7b59a82043fc8b96b890afd460188094b
Gustavo Diques <diquesg@gmail.com>
2025-02-12 13:55:38 -0300

    commit

--- threads-be/src/comments/comments.gateway.ts
+++ threads-be/src/comments/comments.gateway.ts
@@ -1,13 +1,11 @@
-// comments.gateway.ts
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: { origin: '*' } }) // Permitir CORS para evitar bloqueios
 export class CommentsGateway {
     @WebSocketServer()
     server: Server;
 
-    // Método para emitir um novo comentário para todos os clientes conectados
     sendNewComment(comment: any) {
         this.server.emit('newComment', comment);
     }
--- threads-be/src/comments/comments.module.ts
+++ threads-be/src/comments/comments.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-import { CommentsService } from './comments.service';
-import { CommentsController } from './comments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommentsService } from './comments.service';
+import { CommentsGateway } from './comments.gateway';
+import { CommentsController } from './comments.controller';
 import { Comment, CommentSchema } from './schemas/comment.schema';
 
 @Module({
-  controllers: [CommentsController],
-  providers: [CommentsService],
   imports: [
-      MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }])
-  ]
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }])
+  ],
+  controllers: [CommentsController],
+  providers: [CommentsService, CommentsGateway], // Adicione o CommentsGateway como provider
+  exports: [CommentsService, CommentsGateway],   // Exporte para outros módulos se necessário
 })
-export class CommentsModule {}
+export class CommentsModule { }
